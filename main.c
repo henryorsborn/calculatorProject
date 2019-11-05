4b0eb6ae4543d7b496b331f463df894c50cbc23f
henryorsborn <50592835+henryorsborn@users.noreply.github.com>
2019-11-05 14:15:09 -0500

    can now determine step of calculation:

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ char getStep(char *line, char steps[5]);
 
 char * processLine() {
 
-	char steps[5] = {'P', 'E', 'M', 'A', 'T'};
+	char steps[6] = {'P', 'E', 'M', 'A', 'T', 'X'};
 	char step;
 	char *input = (char *)malloc(100 * sizeof(char));
 	gets(input);
@@ -16,18 +16,33 @@ char * processLine() {
 
 }
 
-char getStep(char *line, char steps[5]){
+
+char getStep(char *line, char steps[6]){
 
 	int i;
+	char step='T';
 	for(i = 0; i < strlen(line); i++){
-		
+
 		if(line[i]=='('||line[i]==')'){
 			return 'P';
+		}else if(line[i]=='^') {
+			step='E';
+		}else if((line[i]=='*'||line[i]=='/')&&step!='E') {
+			step='M';
+		} else if((line[i]=='+'||line[i]=='-')&&step!='E'&&step!='M'){
+			step='A';
+		}else if(line[i]==' '){
+			continue;
+		} else {
+			int a;
+			if(!sscanf(&line[i], "%d", &a)){
+				return 'X';
+			}
+
 		}
-		
-	
+
 	}
-	return 'T';
+	return step;
 
 }
 
